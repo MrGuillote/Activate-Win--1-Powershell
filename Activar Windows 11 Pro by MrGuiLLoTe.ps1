89bd0459862bece7550e45876de05ce14b6e4605
Guillermo Quintana <89352244+MrGuillote@users.noreply.github.com>
2023-05-12 21:05:09 -0300

    Create Activar Windows 11 Pro by MrGuiLLoTe.ps1

---
+++ Activar Windows 11 Pro by MrGuiLLoTe.ps1
@@ -0,0 +1,37 @@
+# Comprueba si el usuario actual es un administrador elevado
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+
+# Si el usuario no es un administrador elevado, relanza el script con privilegios elevados
+if (-not $isAdmin)
+{
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
+
+# Verificar la configuración actual de la directiva de ejecución
+Get-ExecutionPolicy
+
+# Establecer la directiva de ejecución en RemoteSigned
+Set-ExecutionPolicy RemoteSigned -Force
+
+# Ejecutar los comandos
+& slmgr.vbs -upk
+
+# Establecer la directiva de ejecución en Unrestricted para permitir la ejecución de scripts 
+Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force
+
+# Ejecutar los comandos
+& slmgr.vbs -upk
+Start-Process powershell.exe -Verb runAs -ArgumentList '-Command', 'slmgr.vbs -upk'
+
+
+Start-Process powershell.exe -Verb runAs -ArgumentList '-Command',
+'slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX' -Wait
+
+Start-Process powershell.exe -Verb runAs -ArgumentList '-Command',
+'slmgr /skms kms.digiboy.ir' -Wait
+
+Start-Process powershell.exe -Verb runAs -ArgumentList '-Command',
+'slmgr /ato' -Wait
+
+Set-ExecutionPolicy Restricted
